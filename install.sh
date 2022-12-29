c6f179f54003a522bb9bc3c1c46d85c27ee26e52
switzerlandnesia <121075751+switzerlandnesia@users.noreply.github.com>
2022-12-29 10:34:11 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+if (( $EUID != 0 )); then
+    echo "Please Run As root"
+    exit
+fi
+
+clear
+
+installTheme(){
+    cd /var/www/
+    tar -cvf PterodactylThemebackup.tar.gz pterodactyl
+    echo "Installing Theme"
+    cd /var/www/pterodactyl
+    rm -r PterodactylTheme
+    git clone https://github.com/switzerlandnesia/PterodactylTheme.git
+    cd PterodactylTheme
+    rm /var/www/pterodactyl/resources/scripts/PterodactylTheme.css
+    rm /var/www/pterodactyl/resources/scripts/index.tsx
+    mv index.tsx /var/www/pterodactyl/resources/scripts/index.tsx
+    mv PterodactylTheme.css /var/www/pterodactyl/resources/scripts/PterodactylTheme.css
+    cd /var/www/pterodactyl
+
+    curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+    apt update
+    apt install -y nodejs
+
+    npm i -g yarn
+    yarn
+
+    cd /var/www/pterodactyl
+    yarn build:production
+    sudo php artisan optimize:clear
+
+
+}
+
+installThemeQuestion(){
+    while true; do
+        read -p "Are You Sure That You Want To Install The Theme [y/n] " yn
+        case $yn in
+            [Yy]* ) installTheme; break;;
+            [Nn]* ) exit;;
+            * ) echo "Please Answer Yes Or No.";;
+        esac
+    done
+}
+
+repair(){
+    bash <(curl https://raw.githubusercontent.com/switzerlandnesia/PterodactylTheme/main/repair.sh)
+}
+
+restoreBackUp(){
+    echo "Restoring Backup"
+    cd /var/www/
+    tar -xvf PterodactylThemebackup.tar.gz
+    rm PterodactylThemebackup.tar.gz
+
+    cd /var/www/pterodactyl
+    yarn build:production
+    sudo php artisan optimize:clear
+}
+echo "Copyright (c) 2022 Angelillo15 | angelillo15.es"
+echo "This program is free software: you can redistribute it and/or modify"
+echo ""
+echo "WhatsApp: https://whatsApp.com"
+echo "Tweet: https://twitter.com"
+echo ""
+echo "[1] Install Theme"
+echo "[2] Restore Backup"
+echo "[3] Repair Panel (Use If You Have An Error In The Theme Installation)"
+echo "[4] Exit"
+
+read -p "Please Enter A Number: " choice
+if [ $choice == "1" ]
+    then
+    installThemeQuestion
+fi
+if [ $choice == "2" ]
+    then
+    restoreBackUp
+fi
+if [ $choice == "3" ]
+    then
+    repair
+fi
+if [ $choice == "4" ]
+    then
+    exit
+fi
